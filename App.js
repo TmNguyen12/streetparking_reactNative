98c4586656daa8f25a0c76ec4344a499bc392f2b
Truong Nguyen <tmnguyen12@gmail.com>
2018-03-20 17:08:39 -0700

    Map works with <view> due to styling

--- App.js
+++ App.js
@@ -2,50 +2,47 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import Map from "./src/components/screens/map";
 import { Location, Permissions } from "expo";
+import { MapView } from "expo";
 
-const deltas = {
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421
-};
+// const deltas = {
+//   latitudeDelta: 0.0922,
+//   longitudeDelta: 0.0421
+// };
 
-const region = {
-  latitude: 37.321996988,
-  longitude: -122.0325472123455,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421
-};
+// const region = {
+//   latitude: 37.321996988,
+//   longitude: -122.0325472123455,
+//   latitudeDelta: 0.0922,
+//   longitudeDelta: 0.0421
+// };
 
 export default class App extends React.Component {
-  state = {
-    region: null
-  };
+  // componentWillMount() {
+  //   this.getLocationAsync();
+  // }
 
-  componentWillMount() {
-    this.getLocationAsync();
-  }
+  // getLocationAsync = async () => {
+  //   let { status } = await Permissions.askAsync(Permissions.LOCATION);
+  //   if (status !== "granted") {
+  //     this.setState({
+  //       errorMessage: "Permission to access location was denied"
+  //     });
+  //   }
 
-  getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== "granted") {
-      this.setState({
-        errorMessage: "Permission to access location was denied"
-      });
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    const region = {
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
-      ...deltas
-    };
-    await this.setState({ region });
-  };
+  //   let location = await Location.getCurrentPositionAsync({});
+  //   const region = {
+  //     latitude: location.coords.latitude,
+  //     longitude: location.coords.longitude,
+  //     ...deltas
+  //   };
+  //   await this.setState({ region });
+  // };
 
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Map region={region} />
-      </SafeAreaView>
+      <View style={styles.container}>
+        <Map />
+      </View>
     );
   }
 }
@@ -53,8 +50,26 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
   }
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     position: "absolute",
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     justifyContent: "flex-end",
+//     alignItems: "center"
+//   },
+//   map: {
+//     position: "absolute",
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0
+//   }
+// });
--- src/components/screens/map.js
+++ src/components/screens/map.js
@@ -5,20 +5,27 @@ import { MapView } from "expo";
 const Marker = MapView.Marker;
 
 export default class Map extends Component {
-  renderMarkers() {
-    return this.props.places.map((place, i) => (
-      <Marker key={i} title={place.name} coordinate={place.coords} />
-    ));
-  }
+  // renderMarkers() {
+  //   return this.props.places.map((place, i) => (
+  //     <Marker key={i} title={place.name} coordinate={place.coords} />
+  //   ));
+  // }
 
   render() {
     const { region } = this.props;
+    console.log("region", region);
     return (
       <MapView
-        styles={styles.container}
-        region={region}
-        showsUserLocation
-        showsMyLocationButton
+        style={styles.container}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        mapType="mutedStandard"
+        showsMyLocationButton={true}
+        zoomEnabled={true}
       />
     );
   }
@@ -26,6 +33,11 @@ export default class Map extends Component {
 const styles = {
   container: {
     width: "100%",
-    height: "80%"
+    height: "80%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   }
 };
